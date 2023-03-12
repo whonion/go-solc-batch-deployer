e5a0680522c89926612846ed58d1496565755b1b
whonion <playregedit@gmail.com>
2023-03-12 15:43:09 +0800

    add go-files and npm dependencies description

---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Connect to the Ethereum network
+	client, err := ethclient.Dial(os.Getenv("RPC_PROVIDER"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Load your private key
+	privateKey, err := crypto.HexToECDSA(os.Getenv("PRIVATE_KEY"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Load and deploy each contract in succession
+	contractFiles, err := ioutil.ReadDir("./contracts")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range contractFiles {
+		if filepath.Ext(file.Name()) == ".sol" {
+			// Compile the contract
+			contractPath := filepath.Join("contracts", file.Name())
+			cmd := exec.Command("solcjs", "--bin", "--abi", "--optimize", "--output-dir", "bin", contractPath)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err = cmd.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Get the absolute path to the bin folder
+			binDir, err := filepath.Abs("./bin")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Rename output files
+			name := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
+			binFilename := fmt.Sprintf("%s.bin", name)
+			abiFilename := fmt.Sprintf("%s.abi", name)
+			binPath := filepath.Join(binDir, fmt.Sprintf("contracts_%s_sol_%s.bin", name, name))
+			abiPath := filepath.Join(binDir, fmt.Sprintf("contracts_%s_sol_%s.abi", name, name))
+			err = os.Rename(binPath, filepath.Join(binDir, binFilename))
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = os.Rename(abiPath, filepath.Join(binDir, abiFilename))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Get the bytecode and ABI from the compiled contract
+			bytecodeFile := filepath.Join(binDir, binFilename)
+			bytecodeBytes, err := ioutil.ReadFile(bytecodeFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			abiFile := filepath.Join(binDir, abiFilename)
+			abiBytes, err := ioutil.ReadFile(abiFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			// Set the gas price and gas limit
+			gasPrice, err := client.SuggestGasPrice(context.Background())
+			if err != nil {
+				log.Fatal(err)
+			}
+			// gasLimit := uint64(120000)
+			// Calculate the gas required for deploying the contract
+			estimateGas, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
+				From: crypto.PubkeyToAddress(privateKey.PublicKey),
+				To:   nil,
+				Data: bytecodeBytes,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Estimated gas: %v\n", estimateGas)
+			gasLimit := estimateGas
+			// Create a new instance of a transaction signer
+			auth := bind.NewKeyedTransactor(privateKey)
+			auth.GasPrice = gasPrice
+			auth.GasLimit = gasLimit //estimateGas +10000
+
+			// Load the contract's ABI
+			contractABI, err := abi.JSON(bytes.NewReader(abiBytes))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Deploy the contract
+			address, tx, _, err := bind.DeployContract(auth, contractABI, bytecodeBytes, client)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Wait for the transaction to be mined
+			fmt.Printf("Contract %s waiting to be mined: 0x%x\n", file.Name(), tx.Hash())
+			receipt, err := bind.WaitMined(context.Background(), client, tx)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if receipt.Status != types.ReceiptStatusSuccessful {
+				log.Fatalf("contract %s deployment failed", file.Name())
+			}
+
+			// Print the contract address and the transaction hash
+			fmt.Printf("Contract %s deployed to: %s\n", file.Name(), address.Hex())
+		}
+	}
+}
